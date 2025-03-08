4957fed19d1c546875a67c6a26a7b4b6cef818f6
benkeepswalking <benkeepsjourney@gmail.com>
2025-03-09 01:34:10 +0800

    Commit 2

--- finance-dashboard/src/App.jsx
+++ finance-dashboard/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import StockForm from './components/StockForm';
+import StockForm from './components/stockForm';
+import StockList from './components/stockList';
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <>
     <h1>My Finance Dashboard</h1>
     <StockForm />
-
+    <StockList />
     </>
   )
 }
---
+++ finance-dashboard/src/components/stockList.jsx
@@ -0,0 +1,22 @@
+import "./dashboardStyling.css";
+
+function StockList() {
+
+    return (
+    <>
+        <div>
+        <h2>Stock List</h2>
+        <p>No stocks added yet</p>
+        </div>
+        <div className="stockItem">
+            <p id="field-symbol">Symbol: AAPL</p>
+            <p>Quantity: 1</p>
+            <p>Purchase Price: 1</p>
+            <p>Current Price: 100</p>
+            <p id="field-profitLoss">Profit/Loss:</p>
+        </div>
+    </>
+    )
+}
+
+export default StockList;
