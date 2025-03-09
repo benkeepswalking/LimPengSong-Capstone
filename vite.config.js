7a22a724f7de52b80f660f3bfa5c5389ddab4ca1
benkeepswalking <benkeepsjourney@gmail.com>
2025-03-09 23:18:34 +0800

    restart deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-/// <reference types="vite/client" />
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/finance-dashboard/',
 })
